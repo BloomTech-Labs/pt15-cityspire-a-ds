b132c320416b1f82f7e163252b2364f1c093300d
scrunt23 <scrunts23@gmail.com>
2021-02-09 21:11:33 -0600

    crime data is in correct data format

--- scripts/crime/csvs/merge_feat_csvs.py
+++ scripts/crime/csvs/merge_feat_csvs.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import string
 
 
 crime_2019 = pd.read_csv('./crime_feat_2019.csv')
@@ -11,5 +12,13 @@ crime_2014 = pd.read_csv('./crime_feat_2014.csv')
 frames = [crime_2019, crime_2018, crime_2017, crime_2016, crime_2015, crime_2014]
 
 combined_crime = pd.concat(frames)
+
+combined_crime = combined_crime.drop(combined_crime.columns[[0, 1, 2, 3, 4, 8]], axis=1)
+
+print(list(combined_crime.columns))
+
+print(combined_crime.dtypes)
+
+combined_crime.to_csv('crime.csv')
 
 print(combined_crime.head(4))
